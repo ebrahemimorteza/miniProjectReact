31fa024ddcc2aea3ff26f265c126ff32ebfcfc71
e._morteza11 <ebrahemimorteza10@gmail.com>
2023-06-05 16:15:24 +0330

    tow commit

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -3,6 +3,12 @@ import TestContext from './context';
 
 const TaskItems = ()=>{
     const {title , setTitle}=useContext(TestContext)
+    const handleChange=(id)=>{
+        var index=title.findIndex(t=>t.id===id)
+        var newTaskItem=[...title]
+        newTaskItem[index].done=!newTaskItem[index].done;
+        setTitle(newTaskItem)
+    }
     return (
         <ul className="list-group m-0 p-0 mt-2">
         {title.map(t=>(
@@ -10,9 +16,9 @@ const TaskItems = ()=>{
                 {t.title}
                 <span>
                 {t.done ?(
-                        <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow "></i>
+                        <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow " onClick={()=>handleChange(t.id)}></i>
                     ):(
-                        <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
+                        <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow" onClick={()=>handleChange(t.id)}></i>
                     )}
                     <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
                 </span>
